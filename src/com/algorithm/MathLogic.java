a6fc5f93c2e14f2f32b382ed7d14e5afafad0231
AL <al@dev.com>
2022-10-14 17:35:07 +0300

    rename method factorial to factorialRecursion

--- src/com/algorithm/MathLogic.java
+++ src/com/algorithm/MathLogic.java
@@ -2,11 +2,11 @@ package com.algorithm;
 
 public class MathLogic {
 
-    public static int factorial(int number){
+    public static int factorialRecursion(int number){
         if(number < 2){
             return 1;
         }else{
-            return number * factorial(number - 1);
+            return number * factorialRecursion(number - 1);
         }
     }
 
