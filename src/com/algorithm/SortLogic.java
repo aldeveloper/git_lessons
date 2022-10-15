b1a0dcd4e68e907a6741b645c375480a95e7e1fe
AL <al@dev.com>
2022-10-15 11:53:57 +0300

    add method bubbleSoort to class SortLogic

--- src/com/algorithm/SortLogic.java
+++ src/com/algorithm/SortLogic.java
@@ -1,4 +1,15 @@
 package com.algorithm;
 
 public class SortLogic {
+
+    public static int[] bubbleSort(int[] array){
+
+        for(int i = 0; i < array.length; i++){
+            for (int j = array.length-1; j > i; j++) {
+
+            }
+        }
+
+        return array;
+    }
 }
