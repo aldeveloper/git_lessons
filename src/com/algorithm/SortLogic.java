d7cde7bfffcf6b6cb03945d7fdb1c15882cc33aa
AL <al@dev.com>
2022-10-15 13:53:52 +0300

    realize method bubble sort

--- src/com/algorithm/SortLogic.java
+++ src/com/algorithm/SortLogic.java
@@ -1,12 +1,19 @@
 package com.algorithm;
 
+import java.util.Arrays;
+
 public class SortLogic {
 
     public static int[] bubbleSort(int[] array){
 
         for(int i = 0; i < array.length; i++){
-            for (int j = array.length-1; j > i; j++) {
+            for (int j = array.length-1; j > i; j--) {
                 int k = 0;
+                if(array[j] < array[i]){
+                    k = array[j];
+                    array[j] = array[i];
+                    array[i] = k;
+                }
             }
         }
 
