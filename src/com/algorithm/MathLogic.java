487affb99f689b9f059867d00a31fca5e95ae6d7
AL <al@dev.com>
2022-10-16 01:27:13 +0300

    add method factorialLoop to class MathLogic

--- src/com/algorithm/MathLogic.java
+++ src/com/algorithm/MathLogic.java
@@ -21,4 +21,19 @@ public class MathLogic {
 
         return Integer.parseInt(binary.reverse().toString());
     }
+
+    public static int factorialLoop(int number){
+
+        if(number < 2){
+            return 1;
+        }
+
+        int result = 1;
+
+        for (int i = 2; i <= number; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
 }
